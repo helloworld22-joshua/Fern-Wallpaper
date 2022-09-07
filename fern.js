5598ae632a24ddfbc9536b93f92542550c6dfba1
Joshua Albrecht <joshua.albrecht@mail.de>
2022-09-07 20:58:33 +0200

    :)

--- fern.js
+++ fern.js
@@ -2,3 +2,19 @@ document.addEventListener("mousemove", (e) => {
     let x = `${e.clientX / window.innerWidth * 100}% ${e.clientY / window.innerHeight * 100}%`;
     document.querySelector("#parallax").style.backgroundPosition = x;
 });
+
+const logo = document.querySelector("#logo");
+
+function livelyPropertyListener(name, val) {
+    switch(name) {
+        case "logoColor":
+            logo.setAttribute("fill", val);
+            break;
+        case "logoSize":
+            logo.style.scale = val;
+            break;   
+        case "hideLogo":
+            logo.style.display = none;
+            break;     
+    }
+}
